be80fedbce17353c905511ac9efe98967eb96667
Slava Mikhalenka <gundenaf@gmail.com>
2023-09-18 14:48:04 +0200

    Added CI/CD task 3

---
+++ Continuous Integration and Continuous Delivery/homework_3/README.md
@@ -0,0 +1,5 @@
+## Goal: Deploy Node.JS App on Linux VM
+
+## Tasks:
+
+Repeat steps from this article, but do this on Linux VM instead of EC2.
---
+++ Continuous Integration and Continuous Delivery/homework_3/pipeline.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent { label 'jenkins-agent' }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git url: 'https://github.com/gundenaf/Project-on-Building-and-Deploying-a-Node.js-Application-with-Docker-on-Ubuntu.git', branch: 'master', credentialsId: '61bb69c1-92ab-4ffb-9732-52b16e8d05d3'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'docker build . -t basanagoudapatil/nodo-todo-app-test:latest'
+            }
+        }
+
+        stage('Test image') {
+            steps {
+                echo 'testing…'
+                sh 'docker inspect basanagoudapatil/nodo-todo-app-test:latest'
+            }
+        }
+
+        stage('Push') {
+            steps {
+                echo 'pushing…'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo 'deploying on another server'
+                sh 'docker stop nodetodoapp || true'
+                sh 'docker rm nodetodoapp || true'
+                echo 'Disclaimer: it is fictional stage, container just launching. DockerHub was removed from this pipeline.'
+                sh 'docker run -d --name nodetodoapp -p 80:80 basanagoudapatil/nodo-todo-app-test:latest'
+            }
+        }
+  }
+}
