04cd46bab8d481ab6051b3260678fb9d73764949
Viachaslau Mikhalenka <gundenaf@gmail.com>
2023-10-06 10:51:02 +0300

    Delete Cloud Computing (AWS)/homework_1/IaC-way directory

--- Cloud Computing (AWS)/homework_1/IaC-way/modules/aws-ec2-instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of EC2 instance"
-  value       = aws_instance.dos_15_mikhalenka_ec2.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP of EC2 instance"
-  value       = aws_instance.dos_15_mikhalenka_ec2.public_ip
-}
--- Cloud Computing (AWS)/homework_1/IaC-way/modules/aws-ec2-vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.dos_15_mikhalenka_vpc.id
-}
-
-output "subnet_id" {
-  description = "ID of the VPC subnet"
-  value       = aws_subnet.dos_15_mikhalenka_subnet.id
-}
